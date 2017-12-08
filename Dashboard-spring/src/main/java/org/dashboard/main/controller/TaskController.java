367c8d47ee06e61b46593a8e7498a9102babaf44
Jacek <howser@wp.pl>
2017-12-08 15:26:32 +0100

    Working version of basic security for /list and /add controllers.

--- Dashboard-spring/src/main/java/org/dashboard/main/controller/TaskController.java
+++ Dashboard-spring/src/main/java/org/dashboard/main/controller/TaskController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Component
 public class TaskController {
 
     @Autowired
