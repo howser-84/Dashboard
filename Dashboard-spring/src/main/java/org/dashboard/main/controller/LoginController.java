661f1d2e1688d620a22dcb3f437a1c8da831abd5
Jacek <howser@wp.pl>
2017-12-11 06:14:31 +0100

    Register url added to controller

--- Dashboard-spring/src/main/java/org/dashboard/main/controller/LoginController.java
+++ Dashboard-spring/src/main/java/org/dashboard/main/controller/LoginController.java
@@ -1,10 +1,10 @@
 package org.dashboard.main.controller;
 
+import org.dashboard.main.data.User;
+import org.dashboard.main.data.UserDAO;
 import org.dashboard.main.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginController {
@@ -12,8 +12,19 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
+    @Autowired
+    private UserDAO userDAO;
+
     @RequestMapping(value = "/login")
     public String login(){
         return "login";
+    }
+
+    @RequestMapping(value = "/register")
+    public @ResponseBody User register(@PathVariable("username") String username, @PathVariable("password") String password){
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setPassword(password);
+        return userDAO.save(newUser);
     }
 }
