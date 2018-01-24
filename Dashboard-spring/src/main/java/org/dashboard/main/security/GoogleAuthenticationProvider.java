fcf7fcc3d479d6e50522c3d0b9141aefc60af6d9
Jacek <howser@wp.pl>
2018-01-24 11:19:05 +0100

    Final, working version with loggin through google and inner database

--- Dashboard-spring/src/main/java/org/dashboard/main/security/GoogleAuthenticationProvider.java
+++ Dashboard-spring/src/main/java/org/dashboard/main/security/GoogleAuthenticationProvider.java
@@ -1,14 +1,32 @@
 package org.dashboard.main.security;
 
+import org.dashboard.main.data.User;
+import org.dashboard.main.data.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
 public class GoogleAuthenticationProvider implements AuthenticationProvider{
+
+    @Autowired
+    private UserDAO userDAO;
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         GoogleAuthenticationToken token = (GoogleAuthenticationToken)authentication;
-        // TODO add token validation here
+
+        String userId = GoogleValidator.getUserID((String) token.getCredentials());
+        if (userId == null){
+            token.setAuthenticated(false);
+            return token;
+        }
+        User user = userDAO.findByUsername(userId);
+        if (user == null){
+            token.setAuthenticated(false);
+            return token;
+        }
+
         token.setAuthenticated(true);
         return token;
     }
